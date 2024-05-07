6ef2f326330d16c9b7a40a32ef25164576240a74
Laborde-Guillemin <Labordeguillem1@gmail.com>
2024-05-07 09:14:50 +0200

    fonctionnement avec des capteurs

--- Serv/src/main.cpp
+++ Serv/src/main.cpp
@@ -10,6 +10,7 @@
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
+#include <Adafruit_BME280.h>
 
 /*Port SPI*/
 #define SCK  18
@@ -18,12 +19,16 @@
 #define CS  32
 
 // Replace with your network credentials
-const char* ssid = "test";
-const char* password = "btsciel24";
+const char* ssid = "WIFI-CIEL"; //WIFI-CIEL
+const char* password = "alcasarciel"; //alcasar-ciel
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 SPIClass spi = SPIClass(VSPI);
+
+Adafruit_BME280 bme;
+float Temp;// bme.readTemperature();
+float Hum;// bme.readHumidity()
 
 void initSDCard(){
   spi.begin(SCK, MISO, MOSI, CS);
@@ -62,21 +67,49 @@ void initWiFi() {
   }
   Serial.println(WiFi.localIP());
 }
+// Initialisation du capteur BME280
+void initBME280(){
+    if (!bme.begin(0x76)) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);
+    }
+}
+/*Basile*/
+void capteurT() {
+    // Mesure des valeurs BME280
+    Temp = bme.readTemperature();
+    Hum = bme.readHumidity();
+
+    Serial.print("Temperature = ");
+    Serial.print(Temp);
+    Serial.println(" °C");
+    Serial.print("Humidité = ");
+    Serial.print(Hum);
+    Serial.println(" %");
+}
 
 void setup() {
   Serial.begin(115200);
   initWiFi();
   initSDCard();
+  initBME280();
+  server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
+    capteurT();
+    String data = "{\"temperature\": " + String(Temp, 2) + ", \"humidity\": " + String(Hum, 2) + "}";
+    request->send(200, "application/json", data);
+  });
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SD, "/index.html", "text/html");
      //server.serveStatic("/Serv/index.html", SD, "text/html");
   });
   server.serveStatic("/", SD, "/");
-
+  server.serveStatic("/styles.css", SD, "/styles.css");
+  server.serveStatic("/script.js", SD, "/script.js");
   server.begin();
 }
 
 void loop() {
-  
+ capteurT();
+ delay(5000);
 }
